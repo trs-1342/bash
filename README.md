8a790124eb0e8c3c997249abc1505a083a13e5bf
trs-1342 <socialmediaoftrs1342@gmail.com>
2026-05-12 19:42:28 +0300

    kullanicidan ad soyad alan ve yazdiran programi yaptim, bir kac eksikligi var giderilecek. README.md dosyasinda da anlattim bir kac anlatim bozuklugu giderme ve iyilestirmeleye ihtiyaci var

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# bash
+# SHELL/BASH
 bu repoda linux sistemini daha iyi anlamam icin shell/bash ogrendiklerim iceriyor.
+
+## echo
+- ekrana yazi yazdirmak icin kullanilir.
+
+## read
+- kullanicidan deger almak icin kullanilir.
+
+## degiskenler
+- $ ile tanimlanirlar, sabit degiskenler buyuk harflerle yazilir.
+- tanimlamak veya yazdirmak icin baslarina $ isareti konulur, ornekler:
+- `$AD`, `$SOYAD`, `$ftp_file='/root/etc/ftp.py'`
--- test/main.sh
+++ test/main.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-echo "selam"
-echo "\n"
+echo "test"
+echo "" # bos satir yapmak icin, henuz tam ogrenmedim.
 echo "selam dunya"
+echo ""
+
+# degisken tanimlanmadan kullanicidan AD degerini aliyorum
+
+echo "ad gir: "
+read AD
+echo -e "girilen ad: $AD"
+
+# degisken tanimlarken kullanicidan SOYAD degerini aliyorum
+
+#$SOYAD # bos oldugu icin uyari veriyor
+
+echo "" # fonksiyon veya \n alt satir karakterlerini bilmeliyim temel olarak
+echo "soyad gir: "
+read SOYAD # basina $ koyarsaniz gecmiste girilen degeri gosteriyor
+echo -e "girilen soyad: $SOYAD" # medium'da 2dk goz atmanin sonucunda -e eklediklerini gordum ve ekledim
